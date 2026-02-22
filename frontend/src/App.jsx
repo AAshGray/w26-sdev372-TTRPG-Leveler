c3cf105f8b6a675e592c93e4a894313f799823ac
AAshGray <193518051+AAshGray@users.noreply.github.com>
2026-02-21 19:38:44 -0800

    added user context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { UserProvider } from './context/UserContext'
 import Header from './components/Header'
 import CharacterList from './components/CharacterList'
 import CharacterDisplay from './components/CharacterDisplay'
+import CharacterCreator from './components/CharacterCreator'
 import './App.css'
+import CreateCharacterCard from './components/CreateCharacterCard'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<CharacterList />} />
-            <Route path="/character/:id" element={<CharacterDisplay />} />
-          </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<CharacterList />} />
+              <Route path="/create/character" element={<CharacterCreator />} />
+              <Route path="/character/:id" element={<CharacterDisplay />} />
+            </Routes>
+          </main>
+        </div>
+      </BrowserRouter>
+    </UserProvider>
   )
 }
 
