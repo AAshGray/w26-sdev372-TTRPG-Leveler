029aeb61ed18d016baf03fb6dbe9b2273519ab35
AAshGray <193518051+AAshGray@users.noreply.github.com>
2026-02-21 23:27:59 -0800

    added User context, useEffect, and authentication to App; Added route for creating characters.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,44 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { UserProvider } from './context/UserContext'
+import { UserProvider, useUser } from './context/UserContext'
+import apiService from './services/apiService'
+import { useEffect } from 'react'
 import Header from './components/Header'
 import CharacterList from './components/CharacterList'
 import CharacterDisplay from './components/CharacterDisplay'
-import CharacterCreator from './components/CharacterCreator'
+import CreateCharacter from './components/CreateCharacter'
 import './App.css'
-import CreateCharacterCard from './components/CreateCharacterCard'
+
+function AppContent() {
+  const { user } = useUser();
+
+  useEffect(() => {
+    if (user?.token) {
+      apiService.setAuthToken(user.token);
+    }
+  }, [user]);
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<CharacterList />} />
+            <Route path="/create/character" element={<CreateCharacter />} />
+            <Route path="/character/:id" element={<CharacterDisplay />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
 
 function App() {
   return (
     <UserProvider>
-      <BrowserRouter>
-        <div className="App">
-          <Header />
-          <main>
-            <Routes>
-              <Route path="/" element={<CharacterList />} />
-              <Route path="/create/character" element={<CharacterCreator />} />
-              <Route path="/character/:id" element={<CharacterDisplay />} />
-            </Routes>
-          </main>
-        </div>
-      </BrowserRouter>
+      <AppContent />
     </UserProvider>
-  )
+  );
 }
 
-export default App
+export default App;
